ebf14f9148cae0a9624758141ed6d893d907c636
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2024-02-09 14:24:55 -0500

    feat: add Header Component

---
+++ app/_components/header.tsx
@@ -0,0 +1,46 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { MenuIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header>
+      <Card>
+        <CardContent className=" p-5 flex items-center justify-between flex-row">
+          <Link href={"/"}>
+            <h1>Weed Wallet</h1>
+            {/* <Image src="/logo.png" alt="Weed Wallet" height={40} width={40} /> */}
+          </Link>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline" size="icon">
+                <MenuIcon size={16} />
+              </Button>
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle>Menu</SheetTitle>
+              </SheetHeader>
+              <SheetDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </SheetDescription>
+            </SheetContent>
+          </Sheet>
+        </CardContent>
+      </Card>
+    </header>
+  );
+};
+
+export default Header;
