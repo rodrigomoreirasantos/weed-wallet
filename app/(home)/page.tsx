ffbb0f89dd1c3b0585221623743034d2fa5ebb54
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2024-03-05 21:20:02 -0500

    feat: add read database information

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,24 +1,18 @@
 import Header from "../_components/header";
 import WeedCard from "../_components/weedCard";
 import Search from "../_components/search";
+import { db } from "../_lib/prisma";
 
-export default function Home() {
+export default async function Home() {
+  const weeds = await db.weed.findMany({});
   return (
     <>
       <Header />
       <Search />
       <div className="overflow-y-auto max-h-screen [&::-webkit-scrollbar]:hidden">
-        <WeedCard />
-        <WeedCard />
-        <WeedCard />
-        <WeedCard />
-        <WeedCard />
-        <WeedCard />
-        <WeedCard />
-        <WeedCard />
-        <WeedCard />
-        <WeedCard />
-        <WeedCard />
+        {weeds.map((weed) => (
+          <WeedCard key={weed.id} weed={weed} />
+        ))}
       </div>
     </>
   );
--- app/_components/weedCard.tsx
+++ app/_components/weedCard.tsx
@@ -1,30 +1,37 @@
 import { Badge } from "@/components/ui/badge";
 import { Card, CardContent } from "@/components/ui/card";
+import { Weed } from "@prisma/client";
 import { Leaf, Pipette } from "lucide-react";
 import Image from "next/image";
 
-const WeedCard = () => {
+interface WeedProps {
+  weed: Weed;
+}
+
+const WeedCard = ({ weed }: WeedProps) => {
   return (
     <div className="p-5">
       <Card className="rounded-lg">
         <CardContent className="flex flex-row gap-5 p-5">
           <Image src="/weedImg.png" alt="Weed Image" height={80} width={130} />
           <div>
-            <Badge className="bg-[#D9D9D9] text-gray-600 rounded">Sativa</Badge>
-            <h2>Amnésia Haze</h2>
+            <Badge className="bg-[#D9D9D9] text-gray-600 rounded">
+              {weed.type}
+            </Badge>
+            <h2>{weed.name}</h2>
             <div className="flex flex-row justify-between">
               <div className="flex flex-col items-center">
                 <p className="text-xs">THC</p>
                 <div className="flex flex-row items-center gap-1">
                   <Leaf size={16} />
-                  <p>19%</p>
+                  <p>{weed.thc}%</p>
                 </div>
               </div>
               <div className="flex flex-col items-center">
                 <p className="text-xs">CBD</p>
                 <div className="flex flex-row items-center gap-1">
                   <Pipette size={16} />
-                  <p>9%</p>
+                  <p>{weed.cbd}%</p>
                 </div>
               </div>
             </div>
