dd0a6eb5276b64bc686f1e395017eca961b900b6
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2024-03-08 21:25:48 -0500

    feat: clear imports

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,7 @@
-import { useSession } from "next-auth/react";
 import Header from "../_components/header";
 import { db } from "../_lib/prisma";
-import AboutUs from "./_components/about-us";
+
 import User from "./_components/user";
-import WeedCard from "../_components/weed-card";
 
 export default async function Home() {
   const weeds = await db.weed.findMany({});
