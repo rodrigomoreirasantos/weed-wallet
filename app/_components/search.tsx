e498d54fd464bbefc118bd298a5ea1a2ed3a2171
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2024-03-26 16:34:53 -0400

    feat: add NewWeed component

--- app/_components/search.tsx
+++ app/_components/search.tsx
@@ -1,14 +1,29 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { SearchIcon } from "lucide-react";
+import { PlusCircle } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 const Search = () => {
   return (
     <div className="p-5 flex flex-row gap-4">
       <Input />
-      <Button className="text-gray-600 bg-[#D9D9D9]">
-        <SearchIcon />
-      </Button>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button className="text-gray-600 bg-[#D9D9D9]">
+            <PlusCircle />
+          </Button>
+        </SheetTrigger>
+
+        <SheetContent side="bottom">
+          <SheetHeader>Add New Weed</SheetHeader>
+          <div></div>
+        </SheetContent>
+      </Sheet>
     </div>
   );
 };
