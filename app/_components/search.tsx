ff07ef28811e4c3186296a658855a298d89fc63d
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2024-03-27 18:11:42 -0400

    feat: add NewWeed component

--- app/_components/search.tsx
+++ app/_components/search.tsx
@@ -1,29 +1,15 @@
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { PlusCircle } from "lucide-react";
-import {
-  Sheet,
-  SheetContent,
-  SheetHeader,
-  SheetTrigger,
-} from "@/components/ui/sheet";
+import { useForm } from "react-hook-form";
+import { Weed } from "@prisma/client";
+import NewWeed from "./new-weed";
 
 const Search = () => {
+  const {} = useForm<Weed>();
+
   return (
     <div className="p-5 flex flex-row gap-4">
       <Input />
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button className="text-gray-600 bg-[#D9D9D9]">
-            <PlusCircle />
-          </Button>
-        </SheetTrigger>
-
-        <SheetContent side="bottom">
-          <SheetHeader>Add New Weed</SheetHeader>
-          <div></div>
-        </SheetContent>
-      </Sheet>
+      <NewWeed />
     </div>
   );
 };
