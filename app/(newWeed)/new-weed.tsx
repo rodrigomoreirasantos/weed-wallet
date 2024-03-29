a4a3c8512b7d2c05737ac6a8c6d76ec35d2cdd1b
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2024-03-29 09:56:03 -0400

    fix: ensure button weed type in the same form

--- app/(newWeed)/new-weed.tsx
+++ app/(newWeed)/new-weed.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
@@ -8,9 +10,12 @@ import {
 } from "@/components/ui/sheet";
 import { PlusCircle } from "lucide-react";
 import { useState } from "react";
-import WeedTypeButton from "./_components/weed-type-button";
 
 const NewWeed = () => {
+  const [currentWeedType, setCurrentWeedType] = useState<string>();
+  function handleWeedType(type: string) {
+    setCurrentWeedType(type);
+  }
   return (
     <Sheet>
       <SheetTrigger asChild>
@@ -21,7 +26,38 @@ const NewWeed = () => {
 
       <SheetContent side="bottom" className="h-auto flex flex-col gap-8">
         <SheetHeader>Add New Weed</SheetHeader>
-        <WeedTypeButton />
+        <div className="flex items-center justify-between">
+          <Button
+            variant="outline"
+            id="sativa"
+            onClick={() => handleWeedType("sativa")}
+            className={`${
+              currentWeedType == "sativa" && "bg-red-500 text-white"
+            }`}
+          >
+            Sativa
+          </Button>
+          <Button
+            variant="outline"
+            id="indica"
+            onClick={() => handleWeedType("indica")}
+            className={`${
+              currentWeedType == "indica" && "bg-red-500 text-white"
+            }`}
+          >
+            Indica
+          </Button>
+          <Button
+            variant="outline"
+            id="hybrid"
+            onClick={() => handleWeedType("hybrid")}
+            className={`${
+              currentWeedType == "hybrid" && "bg-red-500 text-white"
+            }`}
+          >
+            Hybrid
+          </Button>
+        </div>
         <div>
           <Input placeholder="Add  a name for this weed..." />
         </div>
