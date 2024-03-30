9a21e4ee247afd72826f20ae54242bc8b9cff42f
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2024-03-30 13:19:41 -0400

    feat: start form new weed

--- app/_components/new-weed.tsx
+++ app/_components/new-weed.tsx
@@ -10,12 +10,25 @@ import {
 } from "@/components/ui/sheet";
 import { PlusCircle } from "lucide-react";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+interface NewWeedProps {
+  type: string;
+}
 
 const NewWeed = () => {
   const [currentWeedType, setCurrentWeedType] = useState<string>();
+
+  const { register, handleSubmit, setValue } = useForm<NewWeedProps>();
+
+  const onSubmit = (weed: NewWeedProps) => {
+    return console.log(weed);
+  };
+
   function handleWeedType(type: string) {
     setCurrentWeedType(type);
   }
+
   return (
     <Sheet>
       <SheetTrigger asChild>
@@ -28,35 +41,47 @@ const NewWeed = () => {
         <SheetHeader>Add New Weed</SheetHeader>
         <div className="flex items-center justify-between">
           <Button
-            variant="outline"
             id="sativa"
-            onClick={() => handleWeedType("sativa")}
             className={`px-9 ${
               currentWeedType == "sativa" &&
               "bg-weed-sativa hover:bg-weed-sativa"
             }`}
+            variant="outline"
+            value={currentWeedType}
+            {...register("type")}
+            onClick={() => (
+              setValue("type", "sativa"), handleWeedType("sativa")
+            )}
           >
             Sativa
           </Button>
           <Button
-            variant="outline"
             id="indica"
-            onClick={() => handleWeedType("indica")}
             className={`px-9 ${
               currentWeedType == "indica" &&
               "bg-weed-indica hover:bg-weed-indica"
             }`}
+            variant="outline"
+            value={currentWeedType}
+            {...register("type")}
+            onClick={() => (
+              setValue("type", "indica"), handleWeedType("indica")
+            )}
           >
             Indica
           </Button>
           <Button
-            variant="outline"
             id="hybrid"
-            onClick={() => handleWeedType("hybrid")}
             className={`px-9 ${
               currentWeedType == "hybrid" &&
               "bg-weed-hybrid hover:bg-weed-hybrid"
             }`}
+            variant="outline"
+            value={currentWeedType}
+            {...register("type")}
+            onClick={() => (
+              setValue("type", "hybrid"), handleWeedType("hybrid")
+            )}
           >
             Hybrid
           </Button>
@@ -69,7 +94,7 @@ const NewWeed = () => {
           <Input placeholder="Cbd" />
         </div>
 
-        <Button>Save</Button>
+        <Button onClick={() => handleSubmit(onSubmit)()}>Save</Button>
       </SheetContent>
     </Sheet>
   );
