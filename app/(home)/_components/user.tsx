e27259224ef3c6789d485768002b4110b3c46b10
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2024-04-08 09:40:07 -0400

    feat: ensure bring weeds was saved from user

--- app/(home)/_components/user.tsx
+++ app/(home)/_components/user.tsx
@@ -1,35 +1,23 @@
-"use client";
-
-import Search from "@/app/_components/search";
-import WeedCard from "@/app/_components/weed-card";
-import { Weed } from "@prisma/client";
-import { useSession } from "next-auth/react";
 import React from "react";
-import AboutUs from "./about-us";
+import Search from "@/app/_components/search";
+
+import { Weed } from "@prisma/client";
+import WeedCard from "@/app/_components/weed-card";
 
 interface WeedProps {
   weeds: Weed[];
 }
-
 const User = ({ weeds }: WeedProps) => {
-  const { status, data: session } = useSession();
-
   return (
     <div>
-      {status == "authenticated" ? (
-        <>
-          <Search />
-          <div className="overflow-y-auto max-h-screen [&::-webkit-scrollbar]:hidden">
-            {/* {weeds.map((weed) => (
-              <>
-                <WeedCard key={weed.id} weed={weed} />
-              </>
-            ))} */}
-          </div>
-        </>
-      ) : (
-        <AboutUs />
-      )}
+      <Search />
+      <div className="overflow-y-auto max-h-screen [&::-webkit-scrollbar]:hidden">
+        {weeds.map((weed) => (
+          <>
+            <WeedCard key={weed.id} weed={weed} />
+          </>
+        ))}
+      </div>
     </div>
   );
 };
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,20 @@
 import Header from "../_components/header";
-import { db } from "../_lib/prisma";
 
 import User from "./_components/user";
+import { db } from "../_lib/prisma";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../_lib/auth";
 
 export default async function Home() {
-  const weeds = await db.weed.findMany({});
+  const session = await getServerSession(authOptions);
+  const weeds = await db.weed.findMany({
+    where: {
+      userId: (session?.user as any).id,
+    },
+  });
+
+  console.log(weeds);
+
   return (
     <>
       <Header />
