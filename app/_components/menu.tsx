4d9422a9f1d16402f1c2872e54f87e3d5be0a5e1
rodrigo <rodrigomoreirasantos01@gmail.com>
2024-04-22 21:03:51 -0400

    feat: ensure Sheet Component was close after select button in menu

--- app/_components/menu.tsx
+++ app/_components/menu.tsx
@@ -37,7 +37,7 @@ const Menu = () => {
             </Link>
           </SheetClose>
 
-          <SheetClose>
+          <SheetClose asChild>
             <Link href="/favorites">
               <Button
                 variant="outline"
