183a29528d0e7e92afb212c3b5a86a1f2bb3fb7e
rodrigo <rodrigomoreirasantos01@gmail.com>
2024-04-22 21:00:47 -0400

    feat: ensure Sheet Component was close after select button in menu

--- app/_components/menu.tsx
+++ app/_components/menu.tsx
@@ -3,6 +3,7 @@
 import { Button } from "@/components/ui/button";
 import {
   Sheet,
+  SheetClose,
   SheetContent,
   SheetHeader,
   SheetTrigger,
@@ -24,23 +25,31 @@ const Menu = () => {
       <SheetContent>
         <SheetHeader>Menu</SheetHeader>
         <div className="py-5 flex flex-col gap-4">
-          <Link href="/">
-            <Button variant="outline" className={`w-full justify-start gap-2 `}>
-              <Home size={16} />
-              Home
-            </Button>
-          </Link>
-          <Link href="/favorites">
-            <Button
-              variant="outline"
-              className={`w-full justify-start gap-2 ${
-                path.includes("/favorites") && "bg-black text-white"
-              }`}
-            >
-              <Heart size={16} />
-              Favorites
-            </Button>
-          </Link>
+          <SheetClose asChild>
+            <Link href="/">
+              <Button
+                variant="outline"
+                className={`w-full justify-start gap-2 `}
+              >
+                <Home size={16} />
+                Home
+              </Button>
+            </Link>
+          </SheetClose>
+
+          <SheetClose>
+            <Link href="/favorites">
+              <Button
+                variant="outline"
+                className={`w-full justify-start gap-2 ${
+                  path.includes("/favorites") && "bg-black text-white"
+                }`}
+              >
+                <Heart size={16} />
+                Favorites
+              </Button>
+            </Link>
+          </SheetClose>
         </div>
       </SheetContent>
     </Sheet>
