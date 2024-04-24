7b57c6bcc68ba00950c9ac63e97b1983df8a7378
rodrigo <rodrigomoreirasantos01@gmail.com>
2024-04-24 18:14:17 -0400

    feat: ensure Header Component is not duplicity

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,11 +4,12 @@ import User from "./_components/user";
 import { db } from "../_lib/prisma";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../_lib/auth";
+import AboutUs from "./_components/about-us";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
 
-  if (!session) return <Header />;
+  if (!session) return <AboutUs />;
 
   const weeds = await db.weed.findMany({
     where: {
--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,16 +1,11 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../_lib/auth";
-import Header from "../_components/header";
 import { db } from "../_lib/prisma";
 import WeedCard from "../_components/weed-card";
 import FavoritesProvider from "../_providers/favorites";
 
 export default async function Favorites() {
   const session = await getServerSession(authOptions);
-
-  if (!session || !session.user || !(session?.user as any).id) {
-    return <Header />;
-  }
 
   const weeds = await db.weed.findMany({
     where: {
